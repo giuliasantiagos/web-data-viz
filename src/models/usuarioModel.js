d1e3238129cb0b27dbd3b9ec47d3cb60ce348d20
giuliasantiagos <giulia.silva@sptech.school>
2025-05-02 13:24:21 -0300

    adicionando validações no cadastro e cpf na página pós login

--- src/models/usuarioModel.js
+++ src/models/usuarioModel.js
@@ -16,7 +16,7 @@ function cadastrar(nome, email, senha, cpf, fkEmpresa) {
     // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
     //  e na ordem de inserção dos dados.
     var instrucaoSql = `
-        INSERT INTO usuario (nome, email, senha, fk_empresa, cpf) VALUES ('${nome}', '${email}', '${senha}', '${cpf}', '${fkEmpresa}');
+        INSERT INTO usuario (nome, email, senha, cpf, fk_empresa) VALUES ('${nome}', '${email}', '${senha}', '${cpf}', '${fkEmpresa}');
     `;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
